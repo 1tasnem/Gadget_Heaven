23182746fd597224ba401a84ed231cad4622f90a
1tasnem <tasnemprodhan@gmail.com>
2025-05-23 18:02:11 +0600

    final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,28 +2,50 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer bg-neutral text-neutral-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </footer>
+    <footer class="bg-white text-gray-800 py-10 shadow-md">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="text-center mb-8">
+      <h2 class="text-xl font-bold">Gadget Heaven</h2>
+      <p class="text-sm text-gray-500 mt-2">Leading the way in cutting-edge technology and innovation.</p>
+    </div>
+
+    <hr class="border-t border-gray-200 mb-8"/>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5  mx-70 text-sm text-center md:text-left">
+      
+      <div>
+        <h3 class="text-base font-semibold mb-3">Services</h3>
+        <ul class="space-y-2">
+          <li><a href="#" class="hover:underline">Product Support</a></li>
+          <li><a href="#" class="hover:underline">Order Tracking</a></li>
+          <li><a href="#" class="hover:underline">Shipping & Delivery</a></li>
+          <li><a href="#" class="hover:underline">Returns</a></li>
+        </ul>
+      </div>
+
+      
+      <div>
+        <h3 class="text-base font-semibold mb-3">Company</h3>
+        <ul class="space-y-2">
+          <li><a href="#" class="hover:underline">About Us</a></li>
+          <li><a href="#" class="hover:underline">Careers</a></li>
+          <li><a href="#" class="hover:underline">Contact</a></li>
+        </ul>
+      </div>
+
+      
+      <div>
+        <h3 class="text-base font-semibold mb-3">Legal</h3>
+        <ul class="space-y-2">
+          <li><a href="#" class="hover:underline">Terms of Service</a></li>
+          <li><a href="#" class="hover:underline">Privacy Policy</a></li>
+          <li><a href="#" class="hover:underline">Cookie Policy</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
   );
 };
 
